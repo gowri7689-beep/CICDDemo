4d4e81dcaa0334e79688a3cdc8acb76f666a4da0
Usha <gowri7689@gmail.com>
2026-01-28 17:12:38 +0800

    RightCommit

--- CalcDemo/src/test/java/CalculatorTest.java
+++ CalcDemo/src/test/java/CalculatorTest.java
@@ -5,13 +5,13 @@
     public class CalculatorTest {
 
         @Test
-        public void testAdd() {
+        public void testAdd1() {
             Calculator calculator = new Calculator();
             int result = calculator.add1(3, 2);
             assertEquals("The add method should return the sum of 3 and 2", 5, result);
         }
     @Test
-    public void testSub() {
+    public void testSub1() {
         Calculator calculator = new Calculator();
         int result = calculator.sub1(3, 2);
         assertEquals("The sub method should return the sum of 3 and 2", 1, result);
@@ -20,6 +20,6 @@
     public void testMultiply() {
         Calculator calculator = new Calculator();
         int result = calculator.multiply(3, 2);
-        assertEquals("The multiply method should return the sum of 3 and 4",5, result);
+        assertEquals("The multiply method should return the sum of 3 and 4",6, result);
     }
 }
